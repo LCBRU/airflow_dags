d2629135ed494019fbc44ac4af38cc5fe937ccd1
Richard Bramley <rabramley@gmail.com>
2022-08-31 14:41:14 +0000

    Add source DB to redcap instances

--- warehousing/sql/wh_central_merge_data/CREATE__redcap_instances.sql
+++ warehousing/sql/wh_central_merge_data/CREATE__redcap_instances.sql
@@ -2,7 +2,8 @@ IF OBJECT_ID(N'dbo.redcap_instances', N'U') IS NOT NULL
     DROP TABLE warehouse_central.dbo.redcap_instances;
 
 CREATE TABLE warehouse_central.dbo.redcap_instances (
-    datalake_db VARCHAR(255),
+    datalake_database VARCHAR(255),
+    source_database VARCHAR(255),
     redcap_version VARCHAR(255),
     redcap_base_url VARCHAR(2000)
 )
@@ -10,8 +11,19 @@ CREATE TABLE warehouse_central.dbo.redcap_instances (
 EXEC sp_MSforeachdb
 @command1='IF ''?'' LIKE ''datalake_redcap_%''
 BEGIN 
-	INSERT INTO warehouse_central.dbo.redcap_instances (datalake_db, redcap_version, redcap_base_url)
-	SELECT ''?'' datalake_db, *  
+	INSERT INTO warehouse_central.dbo.redcap_instances (datalake_database, source_database, redcap_version, redcap_base_url)
+	SELECT
+        ''?'' datalake_db,
+        CASE ''?''
+            WHEN ''datalake_redcap_uhl'' THEN ''redcap6170_briccs''
+            WHEN ''datalake_redcap_n3'' THEN ''redcap6170_briccsext''
+            WHEN ''datalake_redcap_uol'' THEN ''uol_crf_redcap''
+            WHEN ''datalake_redcap_internet'' THEN ''uol_survey_redcap''
+            WHEN ''datalake_redcap_national'' THEN ''redcap_national''
+            WHEN ''datalake_redcap_easyas'' THEN ''uol_easyas_redcap''
+        END source_db,
+        redcap_version,
+        redcap_base_url
 	FROM  
 	(
         SELECT *   
