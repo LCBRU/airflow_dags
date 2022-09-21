e9cb9f22c21114468016eb685ee3f226cccc3434
Richard Bramley <rabramley@gmail.com>
2022-09-21 14:46:12 +0000

    Refactor creating dags - didnt work

--- extracts/main.py
+++ extracts/main.py
@@ -1,7 +1,23 @@
+import os
+from datetime import timedelta, datetime
+from airflow import DAG
 from extracts.easy_as_severe_screening import create_easy_as_severe_screening_dag
-from tools import create_dag
 
 
-dag = create_dag(title='exports', schedule_name='SCHEDULE_LOAD_WAREHOUSE')
+default_args = {
+    "owner": "airflow",
+    "reties": 3,
+    "retry_delay": timedelta(minutes=5),
+    "start_date": datetime(2020, 1, 1),
+	'email': os.environ.get('ERROR_EMAIL_ADDRESS', '').split(';'),
+	'email_on_failure': True,
+}
+
+dag = DAG(
+    dag_id="exports",
+    schedule_interval=os.environ.get('SCHEDULE_LOAD_WAREHOUSE', None) or None,
+    default_args=default_args,
+    catchup=False,
+)
 
 easy_as_severe_screening = create_easy_as_severe_screening_dag(dag)
