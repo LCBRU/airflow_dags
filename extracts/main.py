146d01a1f6761cd939104494cc5a5b9a00da50d6
Richard Bramley <rabramley@gmail.com>
2022-09-21 13:15:32 +0000

    Debug emailing

--- extracts/main.py
+++ extracts/main.py
@@ -9,6 +9,7 @@ default_args = {
     "reties": 3,
     "retry_delay": timedelta(minutes=5),
     "start_date": datetime(2020, 1, 1),
+	'email': os.environ.get('ERROR_EMAIL_ADDRESS', '').split(';'),
 	'email_on_failure': True,
 }
 
