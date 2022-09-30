f490a70e8ce6e203334fc5f0ede70d97db1cd9d3
Richard Bramley <rabramley@gmail.com>
2022-09-30 15:08:20 +0000

    Remove drop

--- warehousing/sql/wh_central_merge_data/CREATE__redcap_log.sql
+++ warehousing/sql/wh_central_merge_data/CREATE__redcap_log.sql
@@ -1,7 +1,5 @@
 SET QUOTED_IDENTIFIER OFF
 
-DROP TABLE redcap_log
-
 CREATE TABLE redcap_log (
     id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     meta__redcap_field_id INT NOT NULL,
@@ -11,6 +9,7 @@ CREATE TABLE redcap_log (
     action_datetime DATETIME NOT NULL,
     action_type NVARCHAR(500) NOT NULL,
     data_value NVARCHAR(500) NOT NULL,
+    [instance] INT NOT NULL,
     INDEX idx__redcap_log__meta__redcap_field_id (meta__redcap_field_id),
     INDEX idx__redcap_log__meta__redcap_event_id (meta__redcap_event_id),
     INDEX idx__redcap_log__redcap_participant_id (redcap_participant_id),
@@ -31,42 +30,59 @@ CREATE TABLE temp_redcap_log (
     action_datetime DATETIME NOT NULL,
     action_type NVARCHAR(500) NOT NULL,
     field_name NVARCHAR(MAX) NOT NULL,
-    data_value NVARCHAR(MAX) NOT NULL
+    data_value NVARCHAR(MAX) NOT NULL,
+    [instance] INT NOT NULL
 )
 
 EXEC sp_MSforeachdb
 @command1="IF '?' LIKE 'datalake_redcap_%'
 BEGIN
-    INSERT INTO warehouse_central.dbo.temp_redcap_log (datalake_database, project_id, record, event_id, username, action_datetime, action_type, field_name, data_value)
+    INSERT INTO warehouse_central.dbo.temp_redcap_log (datalake_database, project_id, record, event_id, username, action_datetime, action_type, field_name, data_value, [instance])
     SELECT
-        '?' AS datalake_database,
-        x.project_id,
-		x.pk,
-		x.event_id,
-        x.[user] AS username,
-        CONVERT(DATETIME, STUFF(STUFF(STUFF(CONVERT(VARCHAR, x.ts),13,0,':'),11,0,':'),9,0,' ')) action_datetime,
-        x.event AS action_type,
-        TRIM(REPLACE(LEFT(x.data_value, CHARINDEX('=', x.data_value)), '=', '')) AS field_name,
-        TRIM(REPLACE(RIGHT(x.data_value, LEN(x.data_value) - CHARINDEX('=', x.data_value)), CHAR(39), '')) AS data_value
+        '?',
+        project_id,
+		pk,
+		event_id,
+        [user],
+        CONVERT(DATETIME, STUFF(STUFF(STUFF(CONVERT(VARCHAR, ts),13,0,':'),11,0,':'),9,0,' ')),
+        event,
+        TRIM(REPLACE(LEFT(data_value, CHARINDEX('=', data_value)), '=', '')),
+        TRIM(REPLACE(RIGHT(data_value, LEN(data_value) - CHARINDEX('=', data_value)), CHAR(39), '')),
+        [instance]
     FROM (
         SELECT
-            rle.project_id,
-            rle.pk,
-            rle.ts,
-            rle.[user],
-            rle.event,
-            rle.event_id,
+            project_id,
+            pk,
+            ts,
+            [user],
+            event,
+            event_id,
+            [instance],
             REPLACE(REPLACE(value, CHAR(13), ''), CHAR(10), '') data_value
-        FROM [?].dbo.redcap_log_event rle
-            CROSS APPLY STRING_SPLIT(rle.data_values, ',')
-        WHERE rle.object_type = 'redcap_data'
-			AND TRIM(rle.event) IN ('DELETE', 'INSERT', 'UPDATE')
-			AND rle.pk IS NOT NULL
-			AND rle.event_id IS NOT NULL
+        FROM (
+            SELECT *,
+                CASE
+                    WHEN [instance] = 1 THEN data_values
+                    ELSE SUBSTRING(data_values, 16 + LEN(CONVERT(VARCHAR, [instance])), LEN(data_values))
+                END sdv
+            FROM (
+                SELECT
+                    *,
+                    CASE WHEN data_values LIKE '![instance = [1-9]!]%' ESCAPE '!' OR data_values LIKE '![instance = [1-9][0-9]!]%' ESCAPE '!' OR data_values LIKE '![instance = [1-9][1-9][0-9]!]%' ESCAPE '!' THEN
+                        CONVERT(INT, SUBSTRING(data_values, 13, PATINDEX('%[0-9]]%', data_values) - 12))
+                    ELSE 1 END AS [instance]
+                FROM [?].dbo.redcap_log_event
+                WHERE object_type = 'redcap_data'
+                    AND TRIM(event) IN ('DELETE', 'INSERT', 'UPDATE')
+                    AND pk IS NOT NULL
+                    AND event_id IS NOT NULL
+            ) x
+        ) y
+        CROSS APPLY STRING_SPLIT(sdv, ',')
     ) x
 END"
 
-INSERT INTO warehouse_central.dbo.redcap_log (meta__redcap_field_id, meta__redcap_event_id, redcap_participant_id, username, action_datetime, action_type, data_value)
+INSERT INTO warehouse_central.dbo.redcap_log (meta__redcap_field_id, meta__redcap_event_id, redcap_participant_id, username, action_datetime, action_type, data_value, [instance])
 SELECT
     mrf2.id field_id,
     mre.id event_id,
@@ -74,7 +90,8 @@ SELECT
     x.username,
     x.action_datetime,
     x.action_type,
-    x.data_value
+    x.data_value,
+    x.[instance]
 FROM warehouse_central.dbo.temp_redcap_log x
 JOIN warehouse_central.dbo.meta__redcap_instance mri 
     ON mri.datalake_database = x.datalake_database
