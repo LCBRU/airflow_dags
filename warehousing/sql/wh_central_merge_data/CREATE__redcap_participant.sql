4593d61d0396a345db9f5a546e6a4dc3efc4dea9
Richard Bramley <rabramley@gmail.com>
2022-10-10 15:12:30 +0000

    Use merged redcap_data for participants

--- warehousing/sql/wh_central_merge_data/CREATE__redcap_participant.sql
+++ warehousing/sql/wh_central_merge_data/CREATE__redcap_participant.sql
@@ -9,19 +9,15 @@ CREATE TABLE dbo.redcap_participant (
     UNIQUE (meta__redcap_project_id, record)
 );
 
-EXEC sp_MSforeachdb
-@command1="IF '?' LIKE 'datalake_redcap_%'
-BEGIN 
-    INSERT INTO warehouse_central.dbo.redcap_participant (meta__redcap_project_id, record)
-    SELECT DISTINCT
-        mrp.id,
-        rd.record
-    FROM [?].dbo.redcap_data rd
-    JOIN warehouse_central.dbo.meta__redcap_instance mri
-        ON mri.datalake_database = '?'
-    JOIN warehouse_central.dbo.meta__redcap_project mrp 
-        ON mrp.meta__redcap_instance_id = mri.id 
-        AND mrp.redcap_project_id = rd.project_id 
-END"
+INSERT INTO warehouse_central.dbo.redcap_participant (meta__redcap_project_id, record)
+SELECT DISTINCT
+    mrp.id,
+    rd.record
+FROM warehouse_central.dbo.merged__redcap_data rd
+JOIN warehouse_central.dbo.meta__redcap_instance mri
+    ON mri.datalake_database = rd.datalake_database 
+JOIN warehouse_central.dbo.meta__redcap_project mrp 
+    ON mrp.meta__redcap_instance_id = mri.id 
+    AND mrp.redcap_project_id = rd.project_id 
 
 SET QUOTED_IDENTIFIER ON;
