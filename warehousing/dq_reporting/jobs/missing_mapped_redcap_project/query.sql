3fbebec390b82c4f208926926af19d3df6acde0c
Richard Bramley <rabramley@gmail.com>
2023-01-30 12:05:46 +0000

    Re-establish audit collectiuon

--- warehousing/dq_reporting/jobs/missing_mapped_redcap_project/query.sql
+++
@@ -1,7 +0,0 @@
-SELECT cosm.collection_protocol_id, cosm.cfg_study_id
-FROM warehouse_config.dbo.cfg_openspecimen_study_mapping cosm
-WHERE cosm.collection_protocol_id NOT IN (
-	SELECT ocp.identifier
-	FROM warehouse_central.dbo.openspecimen__collection_protocol ocp
-)
-;
