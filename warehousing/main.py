f47e9502b2ff4992fd04dfab6233eda0651f17a0
Richard Bramley <rabramley@gmail.com>
2024-04-04 14:22:16 +0100

    just the first things

--- warehousing/main.py
+++ warehousing/main.py
@@ -3,7 +3,6 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from tools import create_sub_dag_task
 from warehousing.audit import create_audit_dag
-from warehousing.dq_reporting import create_dq_reporting
 from warehousing.warehouse_config import create_wh_central_config
 from warehousing.data_download.crf_manager_download import create_download_crf_manager_studies
 from warehousing.data_download.download_to_mysql import create_download_to_mysql_dag
@@ -17,10 +16,10 @@ def create_download_data(dag):
     parent_subdag = create_sub_dag_task(dag, 'download_data', run_on_failures=True)
 
     download_to_mysql = create_download_to_mysql_dag(parent_subdag.subdag)
-    download_edge_studies = create_download_edge_studies(parent_subdag.subdag)
-    download_crfm_studies = create_download_crf_manager_studies(parent_subdag.subdag)
+    # download_edge_studies = create_download_edge_studies(parent_subdag.subdag)
+    # download_crfm_studies = create_download_crf_manager_studies(parent_subdag.subdag)
 
-    download_edge_studies << download_crfm_studies
+    # download_edge_studies << download_crfm_studies
 
     return parent_subdag
     
@@ -42,12 +41,12 @@ dag = DAG(
 )
 
 download_data = create_download_data(dag)
-datalake_mysql_import = create_datalake_mysql_import_dag(dag)
-legacy_datalake_mysql_import = create_legacy_datalake_mysql_import_dag(dag)
-config = create_wh_central_config(dag)
-warehouse = create_warehouse(dag)
-create_study_warehouses = create_wh_create_studies(dag)
-audit = create_audit_dag(dag)
-# email_dq = create_dq_reporting(dag)
+# datalake_mysql_import = create_datalake_mysql_import_dag(dag)
+# legacy_datalake_mysql_import = create_legacy_datalake_mysql_import_dag(dag)
+# config = create_wh_central_config(dag)
+# warehouse = create_warehouse(dag)
+# create_study_warehouses = create_wh_create_studies(dag)
+# audit = create_audit_dag(dag)
+# # email_dq = create_dq_reporting(dag)
 
-download_data >> legacy_datalake_mysql_import >> datalake_mysql_import >> config >> warehouse >> create_study_warehouses >> audit
+# download_data >> legacy_datalake_mysql_import >> datalake_mysql_import >> config >> warehouse >> create_study_warehouses >> audit
