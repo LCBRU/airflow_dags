833148ee746e5b97b6ffd24ce6b162e4082228e7
Richard Bramley <rabramley@gmail.com>
2024-04-12 08:59:46 +0100

    Restore replication and add backups

--- replication/main.py
+++ replication/main.py
@@ -1,62 +1,67 @@
 import logging
 import os
-import pathlib
-from datetime import datetime
+from datetime import timedelta, datetime
 import subprocess
 from airflow import DAG
-from tools import default_dag_args
+from tools import create_sub_dag_task
 from warehousing.database import LiveDbConnection, ReplicantDbConnection
 from airflow.operators.python_operator import PythonOperator
 
 
 
+default_args = {
+    "owner": "airflow",
+    "reties": 3,
+    "retry_delay": timedelta(minutes=5),
+    "start_date": datetime(2020, 1, 1),
+	'email': os.environ.get('ERROR_EMAIL_ADDRESS', '').split(';'),
+	'email_on_failure': True,
+}
+
+dag = DAG(
+    dag_id="replication",
+    schedule_interval=os.environ.get('SCHEDULE_REPLICATE', None) or None,
+    default_args=default_args,
+    catchup=False,
+    start_date=datetime(2020, 1, 1),
+)
+
 def _replicate_database(db, live_conn, replicant_conn):
     logging.info("_replicate_database: Started")
 
-    params = [
-                'mysqldump',
-                '-h',
-                live_conn.host,
-                '-u',
-                live_conn.login,
-                f'--password={live_conn.password}',
-                '--add-drop-database',
-                # '--column-statistics=0',
-                '--databases',
-                db,
-            ]
-    
-    logging.info(' '.join(params))
+    dump = subprocess.Popen(
+        [
+            'mysqldump',
+            '-h',
+            live_conn.host,
+            '-u',
+            live_conn.login,
+            f'--password={live_conn.password}',
+            '--add-drop-database',
+            '--databases',
+            db,
+        ],
+        bufsize=0,
+        stdout=subprocess.PIPE,
+    )
 
-    with open(pathlib.Path("/backup/test.sql"), "w") as outfile:
-        subprocess.run(
-            params,
-            stdout=outfile,
-        )
+    load = subprocess.Popen(
+        [
+            'mysql',
+            '-h',
+            replicant_conn.host,
+            '-u',
+            replicant_conn.login,
+            f'--password={replicant_conn.password}',
+        ],
+        stdin=dump.stdout,
+        stdout=subprocess.PIPE,
+    )
 
-    # load = subprocess.Popen(
-    #     [
-    #         'mysql',
-    #         '-h',
-    #         replicant_conn.host,
-    #         '-u',
-    #         replicant_conn.login,
-    #         f'--password={replicant_conn.password}',
-    #     ],
-    #     stdin=dump.stdout,
-    #     stdout=subprocess.PIPE,
-    #     text=True,
-    # )
+    dump.stdout.close()
+    result = load.communicate()[0]
 
-    # output, errors = load.communicate()
-    # dump.stdout.close()
-
-    # dump.wait()
-
-    # output, errors = dump.communicate()
-
-    # print(f'errors=')
-    # print(f'output=')
+    print(result)
 
     logging.info("_replicate_database: Ended")
 
@@ -87,18 +92,9 @@ dbs = {
     # 'scratch',
 }
 
+def create_download_to_mysql_dag(dag):
+    parent_subdag = create_sub_dag_task(dag, 'replicate_mysql', run_on_failures=True)
 
-dbs = {
-    'redcap_genvasc',
-}
-
-with DAG(
-    dag_id="replication",
-    schedule_interval=os.environ.get('SCHEDULE_REPLICATE', None) or None,
-    default_args=default_dag_args,
-    catchup=False,
-    start_date=datetime(2020, 1, 1),
-):
     for db in dbs:
         live_conn = LiveDbConnection(db)
         replicant_conn = ReplicantDbConnection(db)
@@ -106,9 +102,14 @@ with DAG(
         PythonOperator(
             task_id=f"replicate__replicate_database__{db}",
             python_callable=_replicate_database,
+            dag=parent_subdag.subdag,
             op_kwargs={
                 'db': db,
                 'live_conn': live_conn,
                 'replicant_conn': replicant_conn,
             },
         )
+    
+    return parent_subdag
+
+create_download_to_mysql_dag(dag)
