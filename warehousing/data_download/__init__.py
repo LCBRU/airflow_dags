624f600bc9ab0324d9741ffa67872b023a9dfb77
Richard Bramley <rabramley@gmail.com>
2026-08-18 17:22:17 +0100

    Remove unused downloads

--- warehousing/data_download/__init__.py
+++ warehousing/data_download/__init__.py
@@ -2,11 +2,9 @@ from datetime import datetime
 import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from warehousing.data_download.crf_manager_download import download_crf_manager_studies
 from warehousing.data_download.download_to_mysql import download_mysql_backup_and_restore
 from warehousing.data_download.edge_download import download_edge_studies
 from tools import default_dag_args
-from airflow.utils.trigger_rule import TriggerRule
 
 
 with DAG(
@@ -25,32 +23,14 @@ with DAG(
         },
     )
     
-    PythonOperator(
-        task_id=f"download_mysql_backup_and_restore__uol_easyas_redcap",
-        python_callable=download_mysql_backup_and_restore,
-        op_kwargs={
-            'destination_database': 'uol_easyas_redcap',
-            'source_url': 'https://easy-as.lbrc.le.ac.uk/publish/redcap.db',
-        },
-    )
-    
-    PythonOperator(
-        task_id=f"download_mysql_backup_and_restore__uol_survey_redcap",
-        python_callable=download_mysql_backup_and_restore,
-        op_kwargs={
-            'destination_database': 'uol_survey_redcap',
-            'source_url': 'https://redcap.lcbru.le.ac.uk/publish/redcap.db',
-        },
-    )
-    
-    PythonOperator(
-        task_id=f"download_mysql_backup_and_restore__uol_crf_redcap",
-        python_callable=download_mysql_backup_and_restore,
-        op_kwargs={
-            'destination_database': 'uol_crf_redcap',
-            'source_url': 'https://crf.lcbru.le.ac.uk/publish/redcap.db',
-        },
-    )
+    # PythonOperator(
+    #     task_id=f"download_mysql_backup_and_restore__uol_survey_redcap",
+    #     python_callable=download_mysql_backup_and_restore,
+    #     op_kwargs={
+    #         'destination_database': 'uol_survey_redcap',
+    #         'source_url': 'https://redcap.lcbru.le.ac.uk/publish/redcap.db',
+    #     },
+    # )
 
 
 with DAG(
@@ -64,10 +44,3 @@ with DAG(
             task_id=f"download_edge_studies",
             python_callable=download_edge_studies,
         )
-        # task_download_crfm_studies = PythonOperator(
-        #     task_id=f"download_crf_manager_studies",
-        #     python_callable=download_crf_manager_studies,
-        #     trigger_rule=TriggerRule.ALL_DONE,
-        # )
-
-        # task_download_edge_studies >> task_download_crfm_studies
