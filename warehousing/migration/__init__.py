8153100f368c96bf9cf4ccc587ba0faf77376e12
Richard Bramley <rabramley@gmail.com>
2026-09-07 17:04:17 +0100

    Make it a DAG

--- warehousing/migration/__init__.py
+++ warehousing/migration/__init__.py
@@ -1,33 +1,44 @@
+import datetime
+from airflow import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from tools import default_dag_args
 
 
-legacydwh_update_databases = SQLExecuteQueryOperator(
-    task_id="legacy_update_databases",
-    conn_id="LEGACY_DWH",
-    database="warehouse_central",
-    sql="EXEC mig__update_datebases;",
-)
+with DAG(
+    dag_id="Copy_live_DB_to_DWH",
+    default_args=default_dag_args,
+    schedule=os.environ.get('SCHEDULE_DATALAKE_LOAD', None) or None,
+    template_searchpath=['/opt/airflow/dags/warehousing/datalake_load/sql/'],
+    start_date=datetime(2020, 1, 1),
+    catchup=False,
+):
+    legacydwh_update_databases = SQLExecuteQueryOperator(
+        task_id="legacy_update_databases",
+        conn_id="LEGACY_DWH",
+        database="warehouse_central",
+        sql="EXEC mig__update_datebases;",
+    )
 
-dwh_update_databases = SQLExecuteQueryOperator(
-    task_id="new_update_databases",
-    conn_id="DWH",
-    database="warehouse_central",
-    sql="EXEC mig__update_datebases;",
-)
+    dwh_update_databases = SQLExecuteQueryOperator(
+        task_id="new_update_databases",
+        conn_id="DWH",
+        database="warehouse_central",
+        sql="EXEC mig__update_datebases;",
+    )
 
-legacydwh_update_tables = SQLExecuteQueryOperator(
-    task_id="legacy_update_tables",
-    conn_id="LEGACY_DWH",
-    database="warehouse_central",
-    sql="EXEC mig__update_tables;",
-)
+    legacydwh_update_tables = SQLExecuteQueryOperator(
+        task_id="legacy_update_tables",
+        conn_id="LEGACY_DWH",
+        database="warehouse_central",
+        sql="EXEC mig__update_tables;",
+    )
 
-dwh_update_tables = SQLExecuteQueryOperator(
-    task_id="new_update_tables",
-    conn_id="DWH",
-    database="warehouse_central",
-    sql="EXEC mig__update_tables;",
-)
+    dwh_update_tables = SQLExecuteQueryOperator(
+        task_id="new_update_tables",
+        conn_id="DWH",
+        database="warehouse_central",
+        sql="EXEC mig__update_tables;",
+    )
 
-legacydwh_update_databases >> legacydwh_update_tables
-dwh_update_databases >> dwh_update_tables
+    legacydwh_update_databases >> legacydwh_update_tables
+    dwh_update_databases >> dwh_update_tables
