7c6aac65d3b3e76d724141c7bf5aa6fc98f872f4
Richard Bramley <rabramley@gmail.com>
2026-09-09 09:15:06 +0100

    Will schedule make it appear?

--- warehousing/migration/__init__.py
+++ warehousing/migration/__init__.py
@@ -8,7 +8,7 @@ from tools import default_dag_args
 with DAG(
     dag_id="Copy_live_DB_to_DWH",
     default_args=default_dag_args,
-    # schedule=os.environ.get('SCHEDULE_DATALAKE_LOAD', None) or None,
+    schedule=os.environ.get('SCHEDULE_BACKUP', None) or None,
     template_searchpath=['/opt/airflow/dags/warehousing/datalake_load/sql/'],
     start_date=datetime(2020, 1, 1),
     catchup=False,
